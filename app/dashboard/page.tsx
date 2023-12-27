523eab72204a5d45251d3f39ac6b6202b9c5ad39
#55263025 <hardilpg123@gmail.com>
2023-12-27 11:39:25 +0700

    first commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-    return <p>Dashboard Page A</p>;
+import { sql } from "@vercel/postgres";
+
+export default async function Cart({
+  params
+} : {
+  params: { user: string }
+}): Promise<JSX.Element> {
+  const { rows } = await sql`SELECT * from revenue`;
+
+  return (
+    <div>
+      {rows.map((row) => (
+        <div key={row.revenue}>
+        {row.revenue}
+        </div>
+      ))}
+    </div>
+  );
 }
